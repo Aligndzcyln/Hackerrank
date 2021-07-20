829a26fa3358059d786c73bee5a875d36230dc36
Aligndzcyln <ceylanali80@gmail.com>
2021-07-20 21:08:54 +0300

    solved dates question of day six

---
+++ 10 days of javascript/Day-6/day-6-dates.js
@@ -0,0 +1,16 @@
+function getDayName(dateString) {
+    let dayName;
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    dayName = days[new Date(dateString).getUTCDay()];
+
+    return dayName;
+}
