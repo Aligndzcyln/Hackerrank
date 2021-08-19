f16f3baa4b2d1676638c9f0a1519efa96119364d
Aligndzcyln <ceylanali80@gmail.com>
2021-08-19 23:25:53 +0300

    Solved day 13 question

---
+++ 30 days of code/day-13-abstract-class.js
@@ -0,0 +1,10 @@
+class MyBook extends Book {
+    constructor(title, author, price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    display() {
+        console.log("Title: " + this.title + "\n" + "Author: " + this.author + "\n" + "Price: " + this.price + "\n")
+    }
+}
