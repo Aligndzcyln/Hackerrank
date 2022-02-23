0568952e4cc2571f806ee14de242d1c0d19110e4
Ali Gündüz Ceylan <34925324+Aligndzcyln@users.noreply.github.com>
2022-02-23 23:33:41 +0300

    Cycle detection problem solved

---
+++ Cycle detection/CycleDetection.cs
@@ -0,0 +1,23 @@
+static bool hasCycle(SinglyLinkedListNode head) {
+
+    if(head == null)
+    {
+        return false;
+    }
+        
+    var slow = head;
+    var fast = head;
+    
+    while(fast != null && fast.next != null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+        
+        if(slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
---
+++ Cycle detection/README.md
@@ -0,0 +1,3 @@
+## Link of the page
+
+https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem?isFullScreen=true
